1a728374d1d12a10753e49af79cf0c11e6306875
Robby Awaldi <robbyawaldi@gmail.com>
2021-02-28 13:35:19 +0700

    refactor sidebar module css

--- web/src/components/admin/SideBar.tsx
+++ web/src/components/admin/SideBar.tsx
@@ -5,9 +5,9 @@ import { sideBarMenu } from '../../static/sideBarMenu'
 import { FaChevronLeft, FaDev } from 'react-icons/fa'
 import useDidMountEffect from '../../utils/useDidMountEffect'
 import { useMeQuery } from '../../generated/graphql'
+import styles from '../../styles/Sidebar.module.css'
 
-interface SideBarProps {
-}
+interface SideBarProps {}
 
 export const SideBar: React.FC<SideBarProps> = ({ }) => {
     const router = useRouter()
@@ -44,34 +44,28 @@ export const SideBar: React.FC<SideBarProps> = ({ }) => {
         })
     }, [minimize])
 
-    const handleMinimize = () => {
-        setMinimize(prev => !prev)
-    }
-
+    const handleMinimize = () => setMinimize(prev => !prev)
 
     return (
-        <nav className={`bg-gradient-to-b from-teal-200 to-blueocean h-full flex flex-col transition-all duration-300 ${minimize ? 'w-16' : 'md:w-1/6'}`}>
-            <div className="flex-none h-20 flex items-center justify-center">
+        <nav className={`${styles.sidebar} ${minimize ? 'w-16' : 'md:w-1/6 w-16'}`}>
+            <div className={styles.logo}>
                 <FaDev />
             </div>
             <ul className="flex-grow">
                 {menu.map((item, index) => (
                     <li key={index}>
                         <Link href={`${item.path}${minimize ? '?minimize' : ''}`}>
-                            <div className={`flex space-x-3 items-center pl-5 h-16 cursor-pointer 
-                                ${item.path == router.pathname ? 'bg-teal-400' : ''}`}>
+                            <div className={`${styles.menu} ${item.path == router.pathname ? 'bg-teal-400' : ''}`}>
                                 <div className="flex-none">
                                     {item.icon}
                                 </div>
-                                <span className={`text-md flex-grow transition duration-150 ${minimize ? 'opacity-0' : ''}`}>{item.title}</span>
+                                <span className={`${styles.titleMenu} ${minimize ? 'opacity-0' : 'md:opacity-100 opacity-0'}`}>{item.title}</span>
                             </div>
                         </Link>
                     </li>
                 ))}
             </ul>
-            <div
-                onClick={handleMinimize}
-                className="w-12 h-12 bg-white shadow-md self-center rounded-full mb-10 flex justify-center items-center cursor-pointer">
+            <div onClick={handleMinimize} className={styles.sideButton}>
                 <FaChevronLeft className={`transition duration-300 ${minimize ? 'transform rotate-180' : ''}`} />
             </div>
         </nav>
