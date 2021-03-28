eee648377d4a61587facaeda2f956aa7234e6c41
Robby Awaldi <robbyawaldi@gmail.com>
2021-03-28 19:14:33 +0700

    outline-none

--- web/src/components/admin/Header.tsx
+++ web/src/components/admin/Header.tsx
@@ -1,8 +1,8 @@
 import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { useLogoutMutation, useMeQuery } from '../../generated/graphql';
 import styles from '../../styles/AdmHeader.module.css'
-import { Button, IconButton } from '@chakra-ui/react'
+import { Button, IconButton, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
 import { FaDoorOpen } from 'react-icons/fa'
 import { AiOutlineUser } from 'react-icons/ai'
 
@@ -25,25 +25,48 @@ export const Header: React.FC<HeaderProps> = ({ }) => {
         }, false)
     }, [toggleDropdown])
 
+    const menuList = useMemo(() => (
+        <MenuList>
+            <MenuItem icon={<FaDoorOpen />} onClick={handleLogout}>
+                Logout
+            </MenuItem>
+        </MenuList>
+    ), [])
+
     return (
         <div className={styles.header}>
             <div className={styles.title}>Admin Siti Hajar</div>
-            <div className={styles.dropdown}>
-                {data?.me ? (
-                    <button className={`${styles.profile} logout`} onClick={() => setToggle((toggle) => !toggle)}>
-                        {data.me.email}
-                        <div className={styles.profileAvatar}>
-                        </div>
-                    </button>
-                ) : null}
-                <div className="md:hidden flex">
-                    <IconButton aria-label="user" variant="ghost" className="logout" icon={<AiOutlineUser />} onClick={() => setToggle((toggle) => !toggle)}/>
-                </div>
-                <div className={`${styles.dropdownContent} ${toggleDropdown ? styles.active : ''}`}>
-                    <Button leftIcon={<FaDoorOpen />} isFullWidth className={`${styles.logoutButton} logout`} onClick={handleLogout}>
-                        Logout
-                    </Button>
-                </div>
+            <div className="md:flex hidden">
+                <Menu>
+                    {({ isOpen }) => (
+                        <>
+                            <MenuButton
+                                isActive={isOpen}
+                                as={Button}
+                                variant="ghost"
+                                rightIcon={<div className={styles.profileAvatar} />}>
+                                {data?.me ? data.me.email : null}
+                            </MenuButton>
+                            {menuList}
+                        </>
+                    )}
+                </Menu>
+            </div>
+            <div className="md:hidden flex">
+                <Menu>
+                    {({ isOpen }) => (
+                        <>
+                            <MenuButton
+                                className="hidden"
+                                isActive={isOpen}
+                                as={IconButton}
+                                icon={<AiOutlineUser />}
+                                variant="ghost">
+                            </MenuButton>
+                            {menuList}
+                        </>
+                    )}
+                </Menu>
             </div>
         </div>
     );
