05addb2550330b001af60a8344e6ff6e63677dd1
Robby Awaldi <robbyawaldi@gmail.com>
2021-04-19 17:16:26 +0700

    create searchbar

---
+++ web/src/components/frontend/molecules/Searchbar.tsx
@@ -0,0 +1,24 @@
+import { IconButton } from '@chakra-ui/button'
+import React from 'react'
+import { BiSearch } from 'react-icons/bi'
+import styles from '../../../styles/frontend/Searchbar.module.css'
+
+interface SearchbarProps { }
+
+export const Searchbar: React.FC<SearchbarProps> = ({ }) => {
+    return (
+        <div className={styles.searchBox}>
+            <input 
+                className="w-full" 
+                type="text" 
+                name="search" 
+                placeholder="Cari..."
+                autoComplete="off" />
+            <IconButton
+                className={styles.searchButton}
+                aria-label="Search"
+                icon={<BiSearch /> }
+            />
+        </div>
+    );
+}
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,10 +1,12 @@
+import { Searchbar } from "../components/frontend/molecules/Searchbar";
 import { Footer } from "../components/frontend/organisms/Footer";
 import { withApollo } from "../utils/withApollo";
 
 const Index = () => {
   return (
     <div>
-      <Footer />
+      <Searchbar />
+      {/* <Footer /> */}
     </div>
   );
 };
