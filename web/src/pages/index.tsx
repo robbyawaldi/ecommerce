99b25ec877e5389f0c653270bd31af7823bbd02b
Robby Awaldi <robbyawaldi@gmail.com>
2021-04-25 16:10:22 +0700

    responsive gallery

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -10,6 +10,7 @@ const Index = () => {
       <Header />
       <main className="md:px-24 px-4">
         <Carousel />
+        <h1 className="text-center bold text-2xl my-6">KOLEKSI TERBARU</h1>
         <Gallery />
       </main>
       <Footer />
