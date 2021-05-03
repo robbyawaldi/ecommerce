3301d78addca2a208c7c452f7549482f59e9c202
Robby Awaldi <robbyawaldi@gmail.com>
2021-05-03 10:21:29 +0700

    fix font

--- web/src/components/frontend/templates/Layouts.tsx
+++ web/src/components/frontend/templates/Layouts.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Footer } from '../organisms/Footer';
 import { Header } from '../organisms/Header';
+import styles from '../../../styles/frontend/Layouts.module.css'
 
 interface layoutProps { }
 
 export const Layouts: React.FC<layoutProps> = ({ children }) => {
     return (
-        <div>
+        <div className={styles.box}>
             <Header />
             <main className="md:px-24 px-4 py-2 overflow-x-hidden">
                 {children}
