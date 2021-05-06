0a02132a09158a2dc6b7a241c700c05dd74466e4
Robby Awaldi <robbyawaldi@gmail.com>
2021-05-06 11:12:07 +0700

    integrate product data in card component

--- web/src/components/frontend/atoms/Card.tsx
+++ web/src/components/frontend/atoms/Card.tsx
@@ -1,19 +1,23 @@
+import toRupiah from '@develoka/angka-rupiah-js';
+import { useRouter } from 'next/router';
 import React from 'react'
 import { Product } from '../../../generated/graphql';
 import styles from '../../../styles/frontend/Card.module.css'
 import { textLimit } from '../../../utils/textLimit';
 
-interface CardProps { 
+interface CardProps {
     product: Product
 }
 
 export const Card: React.FC<CardProps> = ({ product }) => {
+    const router = useRouter()
+
     return (
         <div className={styles.card}>
-            <img src={product.images[0].url} />
+            <img className={styles.img} src={product.images[0].url} />
             <h1>{product.title}</h1>
             <p>{textLimit(product.description)}</p>
-            <div>{product.price}</div>
+            <div>{toRupiah(product.price, { floatingPoint: 0 })}</div>
         </div>
     );
 }
