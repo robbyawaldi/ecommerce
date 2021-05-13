72c85e4a35eea646778c44af39e0820ae735cd0f
Robby Awaldi <robbyawaldi@gmail.com>
2021-05-13 19:59:59 +0700

    create greeting

--- web/src/utils/generateText.ts
+++ web/src/utils/generateText.ts
@@ -5,14 +5,28 @@ export type Item = {
     qty: number
 }
 
-const HEADER = `Selamat Siang, Saya ingin memesan dengan rincian sebagai berikut ;;-----------------------;;`
-const FOOTER = `;-----------------------;Terima Kasih`
+export const getGreeting = (): string => {
+    const today = new Date()
+    const hours = today.getHours()
+
+    if (hours < 12)
+        return 'Pagi'
+    else if (hours < 16)
+        return 'Siang'
+    else if (hours < 18)
+        return 'Sore'
+    else
+        return 'Malam'
+}
 
 export const generateText = (items: Item[], subTotal: string): string => {
+    const HEADER = `Selamat ${getGreeting()}, Saya ingin memesan dengan rincian sebagai berikut ;;-------------------------------------------------------------;;`
+    const FOOTER = `;-------------------------------------------------------------;Terima Kasih`
+
     let text = HEADER;
-    for (const { title, size, price, qty} of items) {
+    for (const { title, size, price, qty } of items) {
         text = text + `${title} ukuran ${size} @${price} x ${qty};;`
     }
-    text = text + `;-----------------------;Total ${subTotal}`
+    text = text + `;-------------------------------------------------------------;Total ${subTotal}`
     return (text + FOOTER).replace(/[;]/g, '%0a')
 }
