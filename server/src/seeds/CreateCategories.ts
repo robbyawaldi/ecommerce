77b53e6046d80cdfcfcacb794297353abd0e5efd
Robby Awaldi <robbyawaldi@gmail.com>
2021-06-12 17:51:16 +0700

    fix seeding

--- server/src/seeds/CreateCategories.ts
+++ server/src/seeds/CreateCategories.ts
@@ -1,9 +1,10 @@
 import { Connection } from "typeorm";
 import { Factory, Seeder } from "typeorm-seeding";
-import { Category } from "../entities/Category";
+// import { Category } from "../entities/Category";
 
 export default class CreateAdmin implements Seeder {
     public async run(_: Factory, connection: Connection): Promise<any> {
+        connection.isConnected
     //     await connection
     //         .createQueryBuilder()
     //         .delete()
